46c0732ec403c47bb292dfc25a267c1055d4c509
literallyethan <ethan.k.purser@gmail.com>
2024-04-14 22:48:09 -0500

    sets up environment for Ethanol and prepares work for lexer module

---
+++ Ethanol/src/lexer.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#include "../include/lexer.h"
+
+void test_print() {
+	puts("Hello, World!");
+
+}
---
+++ Ethanol/src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#include "../include/lexer.h"
+
+int main() {
+	test_print();
+
+	return 0;
+}
